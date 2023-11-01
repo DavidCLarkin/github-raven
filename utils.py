5a22cf63c4b87d675d6b2594c6e7c39da630a8cc
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2023-11-01 15:09:36 -0700

    🎉 Add method to detect Slack API retries

---
+++ utils.py
@@ -0,0 +1,11 @@
+def _is_retry(request, logger):
+    """
+    Helper method to check request headers for the X-Slack-Retry-Num header,
+    and return True if the value is greater than 0
+    """
+    retry_number = request.get("headers", {}).get("X-Slack-Retry-Num")
+    logger.info(f'RETRY NUMBER: {retry_number}')
+    if isinstance(retry_number, int) and retry_number > 0:
+        return True
+    else:
+        return False
