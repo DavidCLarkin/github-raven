edd4670d241a0dd74f96d61fec2d45c41b3e6554
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2023-11-01 15:32:16 -0700

    🐛 Get the headers from request object directly

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ def _is_retry(request, logger):
     Helper method to check request headers for the X-Slack-Retry-Num header,
     and return True if the value is greater than 0
     """
-    retry_number = request.get("headers", {}).get("X-Slack-Retry-Num")
+    retry_number = request.headers.get("X-Slack-Retry-Num")
     logger.info(f'RETRY NUMBER: {retry_number}')
     if isinstance(retry_number, int) and retry_number > 0:
         return True
