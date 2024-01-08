13f5e7a8e279262d5c9c1d950069e4d52c559b98
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2024-01-08 11:28:32 -0800

    ✨ Update util function to always log header

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ def _is_retry(request, logger):
     and return True if the value is greater than 0
     """
     retry_number = request.headers.get("X-Slack-Retry-Num", 0)
+    logger.info(f'RETRY NUMBER: {retry_number}')
     if retry_number > 0:
-        logger.info(f'RETRY NUMBER: {retry_number}')
         return True
     else:
         return False
